19e6045cf2b35883e3ab1d37356f1cafb51b04a6
daoson12 <daoduvictor48@gmail.com>
2022-09-18 13:32:22 +0000

    added security folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './layouts/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './security/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  {path:'', component:DashboardComponent}
+  {path:'', component:DashboardComponent},
+  {path:'***', component:PageNotFoundComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { SignInComponent } from './security/sign-in/sign-in.component';
+import { SignUpComponent } from './security/sign-up/sign-up.component';
+import { PageNotFoundComponent } from './security/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    SignInComponent,
+    SignUpComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
